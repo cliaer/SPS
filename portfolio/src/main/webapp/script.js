1cb1bdd5190a96b93b84062ca0c4e07ce2c52ffa
Claire <cliaer3@gmail.com>
2020-07-09 16:05:39 -0700

    Add sefie and basic introduction

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Hello world!', 'surprise, its a wild claire!', '你好，世界！', 'i really like bunnies and birbs'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
